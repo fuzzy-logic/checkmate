c96f3bed5b562f3845c5923b882762316be51103
gawain <gawain.hammond@gmail.com>
2017-12-15 16:03:08 +0000

    added clickable tasks

--- public/checklist.js
+++ public/checklist.js
@@ -1,4 +1,7 @@
 
+	const ESCAPE_KEY = 27;
+	const ENTER_KEY = 13;
+
 var Checklist = React.createClass({
     addTask: function() {
       console.log('addTask()');
@@ -14,13 +17,6 @@ var Checklist = React.createClass({
           tasks: [],
         }
       }
-    },
-    setChecked: function(event) {
-      console.log('setChecked()');
-      /*this.setState({
-          new_name: event.target.value,
-          items: this.state.name
-      });*/
     },
     saveName: function(event) {
       console.log('saveName()');
@@ -43,24 +39,25 @@ var Checklist = React.createClass({
           checklist: checklist
       });
     },
+    handleEnterKey: function(target) {
+      console.log('handleEnterKey()');
+      if(target.charCode === ENTER_KEY){
+        this.addTask(); 
+      }
+    },
     render: function() {
-      const tasks = this.state.checklist.tasks;
-      const taskList = tasks.map((task) => <li>{task.name}</li>  );
       return (
       <div class="checklist">
         <div>
-          <h1>{this.state.checklist.name}: Items: {this.state.checklist.tasks.length}</h1>   
-          <input type="text" placeholder="enter new checklist name" onChange={this.saveName}/>
-          <button type="button" onClick={this.rename}>rename</button>
+          <h1 onClick={this.editName} class="jumbotron">{this.props.name}:</h1> <h1> Items: {this.state.checklist.tasks.length}</h1>   
         </div>
         <div>
-          <input type="text" placeholder="description" onChange={this.newTask}/>
-          <button type="button" onClick={this.addTask}>add task</button>
-          <input type="checkbox" id="item" name="checkmark" value="itemX" onClick={this.setChecked}/>
+          <input type="text" placeholder="enter new task" onChange={this.newTask} onKeyPress={this.handleEnterKey}/>
+
         </div>  
         <div>
-          <ul>{ taskList }</ul>
-        </div>
+        <TaskList  tasks={this.state.checklist.tasks}/>
+      </div>
    
       </div>
       )
@@ -68,7 +65,70 @@ var Checklist = React.createClass({
   });
 
 
+
+  var TaskList = React.createClass({
+
+    render: function() {
+      const rows = [];
+      this.props.tasks.forEach((task) => {
+          rows.push(
+            <Task
+              name={task.name}
+              done={task.done}/>
+          );
+      });
+      return (<ul>{rows}</ul>  );
+    }
+
+  });
+
+
+
+  var Task = React.createClass({
+    toggleDone: function(event) {
+      console.log('toggleDone() done: ' + this.state.done);
+      const toggled =  (! this.state.done);
+      this.setState({
+          done: toggled,
+          name: this.props.name
+      });
+    },
+    getInitialState: function () {
+      //console.log('toggleDone() done: ' + this.state.done);
+			return {name: this.props.name,  done: false};
+		},
+    render: function() {
+
+      var checkedStyle = {
+        marginLeft: '-20px',
+        paddingLeft: '20px',
+        fontSize: '32px',
+        color: '#333',
+        backgroundColor: '#F99',
+        fontWeight:'bold'
+      };
+
+      var uncheckedStyle = {
+        marginLeft: '-20px',
+        paddingLeft: '20px',
+        fontSize: '32px',
+        color: '#333',
+        backgroundColor: '#FFF',
+        fontWeight:'bold'
+      };
+
+      const name = this.props.name;
+      const isDone = this.state.done;
+      if (isDone) {
+        return (<li  style={checkedStyle} onClick={this.toggleDone}> <div class="completedTask" > {name} </div> </li> );
+      } else {
+        return (<li  style={uncheckedStyle} onClick={this.toggleDone}> <div class="completedTask" > {name} </div> </li> );
+      }
+      
+    }
+  });
+
   ReactDOM.render(
-    <Checklist />,
+    <Checklist name="my checklist"/>,
     document.getElementById('app')
   );
